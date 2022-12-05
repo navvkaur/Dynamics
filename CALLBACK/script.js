a7ee04b7d2fd9327edabcec685e537bd0c620470
navvkaur <71594057+navvkaur@users.noreply.github.com>
2022-12-05 12:46:26 +0530

    Update script.js

--- CALLBACK/script.js
+++ CALLBACK/script.js
@@ -1,18 +1,18 @@
 
-var ti = 0;
+
 const posts=[
-    {title:'post one' , body:'this is post one'},
-    {title:'post two' , body:'this is post two'},
+    {title:'post one' , body:'this is post one',createdAt: new Date().getTime()},
+    {title:'post two' , body:'this is post two',createdAt: new Date().getTime()}
 ];
 
-setInterval(ti++,1000);
+
 
 function getPosts()
 {
     setTimeout(() =>{
         let output = ' ';
         posts.forEach((post, index)=>{
-            output += `<li> ${post.title} "created on" ${ti} </li>`;
+            output += `<li> ${post.title} UPDATED ON ${(new Date().getTime()-post.createdAt)/1000} </li>`;
         });
     
     document.body.innerHTML=output;
@@ -22,7 +22,7 @@ function getPosts()
 function createPost(post,callback)
 {
     setTimeout(() =>{
-        posts.push(post);
+        posts.push({...post,createdAt: new Date().getTime() });
         callback();
 
     },2000);
@@ -30,11 +30,11 @@ function createPost(post,callback)
 function create4thPost(post,callback)
 {
     setTimeout(() =>{
-        posts.push(post);
+        posts.push( {...post,createdAt: new Date().getTime() });
         callback();
 
     },3000);
 }
-createPost({title:'post three' , body:'this is post three'},getPosts);
-create4thPost({title:'post four' , body:'this is post four'},getPosts);
+createPost({title:'post three' , body:'this is post three',createdAt:new Date().getTime()},getPosts);
+create4thPost({title:'post four' , body:'this is post four',createdAt:new Date().getTime()},getPosts);
 
